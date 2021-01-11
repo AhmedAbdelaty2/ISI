ce90cd541d17cb621bb64c88b57a18bd28f6d724
Ahmed <justgoand015@gmail.com>
2021-01-11 18:18:45 +0200

    updated the code and made it makes more sense

--- ISI.m
+++ ISI.m
@@ -3,25 +3,27 @@ L = 10; %number of pathes
 x = round(rand(1,N)); 
 x = (2 * x) - 1; %BPSK symbols
 X = ones(L, N);
-X = x.*X; %the real message in different channels
+X = x.*X; %the real message in different pathes
 
-h = MultipathChannel(L,1); %channels effect
+h = MultipathChannel(L,1); %channel pathes effect
 
 H = tril(toeplitz(h));
 H_inv = inv(H);
 
-SNR = [-15 : 0];
+Eb = 1; %energy per bit
+SNR = [-15 : 0]; %different values for signal to noise ratio
 
 for l = 1 : length(SNR)
   
-  No = 1/(10**(SNR(l)/10))
-  n = No/2 * randn(size(X)); %generating noise
+  No = Eb/(10**(SNR(l)/10))
+  n = No/2 * randn(size(X)); %generating noise element
 
-  Y = H * X + n;
-  X_new = H_inv * Y;
+  Y = H * X + n;             %deleverd message
+  X_new = H_inv * Y;         %deleverd message after removing pathes effect
 
   counter = size(X_new, 1) * size(X_new, 2);
 
+  %making desision part
   for i = 1 : counter
     if X_new(i) >= 0
       X_new(i) = 1;
@@ -30,15 +32,11 @@ for l = 1 : length(SNR)
     end
   end
 
-  %X
-  %X_new
-
+  %computing bit error rate
   Error = abs(X - X_new);
   cnt_Error = sum(Error(Error==2)) / 2;
   BER(l) = cnt_Error / counter;
 end
-
-BER
 
 figure;
 plot(SNR, BER,'d-r','linewidth',3);
